c45f611f900eee55cb9bfd42c793406b1eb7c09d
Stepan Svystun <s916ua@gmail.com>
2023-07-20 17:25:41 +0300

    new

--- kateanopriienko/src/js/component.js
+++ kateanopriienko/src/js/component.js
@@ -1,11 +1,3 @@
-/**
- * jQuery-viewport-checker - v1.8.8 - 2017-09-25
- * https://github.com/dirkgroenen/jQuery-viewport-checker
- *
- * Copyright (c) 2017 Dirk Groenen
- * Licensed MIT <https://github.com/dirkgroenen/jQuery-viewport-checker/blob/master/LICENSE>
- */
-
 !function(a){a.fn.viewportChecker=function(b){var c={classToAdd:"visible",classToRemove:"invisible",classToAddForFullView:"full-visible",removeClassAfterAnimation:!1,offset:100,repeat:!1,invertBottomOffset:!0,callbackFunction:function(a,b){},scrollHorizontal:!1,scrollBox:window};a.extend(c,b);var d=this,e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()};return this.checkElements=function(){var b,f;c.scrollHorizontal?(b=Math.max(a("html").scrollLeft(),a("body").scrollLeft(),a(window).scrollLeft()),f=b+e.width):(b=Math.max(a("html").scrollTop(),a("body").scrollTop(),a(window).scrollTop()),f=b+e.height),d.each(function(){var d=a(this),g={},h={};if(d.data("vp-add-class")&&(h.classToAdd=d.data("vp-add-class")),d.data("vp-remove-class")&&(h.classToRemove=d.data("vp-remove-class")),d.data("vp-add-class-full-view")&&(h.classToAddForFullView=d.data("vp-add-class-full-view")),d.data("vp-keep-add-class")&&(h.removeClassAfterAnimation=d.data("vp-remove-after-animation")),d.data("vp-offset")&&(h.offset=d.data("vp-offset")),d.data("vp-repeat")&&(h.repeat=d.data("vp-repeat")),d.data("vp-scrollHorizontal")&&(h.scrollHorizontal=d.data("vp-scrollHorizontal")),d.data("vp-invertBottomOffset")&&(h.scrollHorizontal=d.data("vp-invertBottomOffset")),a.extend(g,c),a.extend(g,h),!d.data("vp-animated")||g.repeat){String(g.offset).indexOf("%")>0&&(g.offset=parseInt(g.offset)/100*e.height);var i=g.scrollHorizontal?d.offset().left:d.offset().top,j=g.scrollHorizontal?i+d.width():i+d.height(),k=Math.round(i)+g.offset,l=g.scrollHorizontal?k+d.width():k+d.height();g.invertBottomOffset&&(l-=2*g.offset),k<f&&l>b?(d.removeClass(g.classToRemove),d.addClass(g.classToAdd),g.callbackFunction(d,"add"),j<=f&&i>=b?d.addClass(g.classToAddForFullView):d.removeClass(g.classToAddForFullView),d.data("vp-animated",!0),g.removeClassAfterAnimation&&d.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend",function(){d.removeClass(g.classToAdd)})):d.hasClass(g.classToAdd)&&g.repeat&&(d.removeClass(g.classToAdd+" "+g.classToAddForFullView),g.callbackFunction(d,"remove"),d.data("vp-animated",!1))}})},("ontouchstart"in window||"onmsgesturechange"in window)&&a(document).bind("touchmove MSPointerMove pointermove",this.checkElements),a(c.scrollBox).bind("load scroll",this.checkElements),a(window).resize(function(b){e={height:a(c.scrollBox).height(),width:a(c.scrollBox).width()},d.checkElements()}),this.checkElements(),this}}(jQuery);
 
 $( document ).ready(function() {
@@ -78,7 +70,11 @@ $( document ).ready(function() {
     classToAdd: 'visible animated fadeInRight', 
     offset: '0%'
   });
-  //=====================
+  $('.left').addClass("hidden_animation").viewportChecker({
+    classToAdd: 'visible animated fadeInLeft', 
+    offset: '0%'
+  });
+
 
   // Burger-main
   $('.drop-btn').click(function() {
@@ -89,13 +85,13 @@ $( document ).ready(function() {
   });
   
   window.addEventListener("click", function(event) {
-    var dropdowns = document.getElementsByClassName("nav-box-right");
-    var containers = document.getElementsByClassName("blok");
+    let dropdowns = document.getElementsByClassName("nav-box-right");
+    let containers = document.getElementsByClassName("blok");
     
     if (!event.target.matches('.drop-btn') && !event.target.matches('.bar1') && !event.target.matches('.bar2') && !event.target.matches('.bar3') && !event.target.matches('.blok')) {
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        var container = containers[i];
+      for (let i = 0; i <dropdowns.length; i++) {
+        let openDropdown = dropdowns[i];
+        let container = containers[i];
         if (openDropdown.classList.contains('show')) {
           openDropdown.classList.remove('show');
           container.classList.remove('change');
@@ -103,7 +99,7 @@ $( document ).ready(function() {
       }
     }
   });
-  //=====================
+
 
   // Language
   const mainFlagDesktop = document.querySelector('.language-switch.desktop .main-flag');
@@ -140,111 +136,56 @@ $( document ).ready(function() {
     });
   });
 
-/*   const mainFlag = document.querySelector('.main-flag');
-  const languageMenu = document.querySelector('.language-menu');
-
-  mainFlag.addEventListener('click', () => {
-    languageMenu.style.display = languageMenu.style.display === 'none' ? 'block' : 'none';
-  });
-  const flags = document.querySelectorAll('.language-flag:not(.main-flag)');
-
-  flags.forEach(flag => {
-    flag.addEventListener('click', () => {
-
-      mainFlag.src = flag.src;
-      mainFlag.alt = flag.alt;
-
-      languageMenu.style.display = 'none';
-    });
-  }); */
-  //=====================
 
   // КНОПКА "ПОКАЗАТИ БІЛЬШЕ" - "ПРИХОВАТИ"
-  var showMoreBtn = document.getElementById('showMoreBtn');
-  var projectCards = document.querySelectorAll('.project-card');
+  let showMoreBtn = document.getElementById('showMoreBtn');
+  let projectCards = document.querySelectorAll('.project-card');
   // Приховує всі блоки, крім перших 4
-  for (var i = 4; i < projectCards.length; i++) {
+  for (let i = 4; i <projectCards.length; i++) {
     projectCards[i].classList.add('project-hidden');
   }
   showMoreBtn.addEventListener('click', function() {
     // Перевіряємо, чи є хоча б один прихований блок
-    var hiddenBlocks = document.querySelectorAll('.project-card.project-hidden');
+    let hiddenBlocks = document.querySelectorAll('.project-hidden');//.project-card 
     if (hiddenBlocks.length > 0) {
         // Показуємо всі приховані блоки
-        for (var i = 0; i < hiddenBlocks.length; i++) {
+        for (let i = 0; i <hiddenBlocks.length; i++) {
           hiddenBlocks[i].classList.remove('project-hidden');
         }
         showMoreBtn.textContent = 'Приховати';
     } else {
         // Приховуємо всі блоки, крім перших двох
-        for (var i = 4; i < projectCards.length; i++) {
+        for (let i = 4; i <projectCards.length; i++) {
           projectCards[i].classList.add('project-hidden');
         }
         showMoreBtn.textContent = 'Показати більше';
     }
   });
-  //=====================
+
 
   // ПАНЕЛЬ СОРТУВАННЯ НА СТОРІНЦІ - "project"
-  const buttons = document.querySelectorAll('.page-project-btn');
-  const projects = document.querySelector('.projects');
+  const tabButtons = document.querySelectorAll('.tab-button');
+  const tabItems = document.querySelectorAll('.tab-item');
 
-  buttons.forEach((button) => {
+  // Add event listeners to each tab button
+  tabButtons.forEach(button => {
     button.addEventListener('click', () => {
-      buttons.forEach((btn) => btn.classList.remove('active'));
-      button.classList.add('active');
-      const filter = button.getAttribute('data-filter');
-      filterProjects(filter);
+      const tab = button.getAttribute('data-tab');
+
+      // Hide all tab items
+      tabItems.forEach(item => {
+        item.classList.remove('active');
+      });
+
+      // Show the selected tab item
+      const selectedTabItem = document.querySelector(`[data-tab="${tab}"]`);
+      selectedTabItem.classList.add('active');
     });
   });
 
-  function filterProjects(filter) {
-    // Виконати сортування проектів тут в залежності від обраного фільтру
-    // Наприклад, ви можете використовувати класи CSS або додавати/видаляти елементи DOM
-    // Приклад: якщо filter === 'graphic', приховати всі проекти, крім тих, що мають клас 'graphic'
-  }
-  //=====================
-
-  
-  /*  //ЗНИКАННЯ ІКОНОК У INPUT
-  // Отримуємо посилання на всі поля вводу та зображення
-  const inputFields = document.querySelectorAll('input');
-  const imageElements = document.querySelectorAll('.input-ico');
-
-  // Функція для зміни видимості зображень
-  function toggleImageVisibility(index) {
-    if (inputFields[index].value.trim() === '') {
-      imageElements[index].style.display = 'inline';
-    } else {
-      imageElements[index].style.display = 'none';
-    }
-  }
-
-  // Слухаємо подію введення тексту у кожному полі вводу
-  inputFields.forEach((inputField, index) => {
-    inputField.addEventListener('input', () => toggleImageVisibility(index));
-    // Викликаємо функцію один раз для початкового значення
-    toggleImageVisibility(index);
-  }); */
-  //=====================
-
-  /* $(window).scroll(function () {
-    return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
-  }); */
-  //=====================
-  
-  // Scroll
-  /* $('.scroll').click(function (e) {
-    event.preventDefault();
-    var id = $(this).attr('href'),
-      top = $(id).offset().top;
-  
-    $('body,html').animate({
-      scrollTop: top - 90
-    }, 500);
-  
-  }); */
-  
+  // Show the default tab on page load (You can set any tab as default by adding the 'active' class to it in HTML)
+  /* const defaultTab = document.querySelector('.tab-item.active');
+  defaultTab.classList.add('active'); */
 
 }) 
 
